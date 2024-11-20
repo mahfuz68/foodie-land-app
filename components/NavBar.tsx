b171695ccaa32b0c5d717eea18a27ce6145ff43c
Mahfuz Anam <94157542+mahfuz68@users.noreply.github.com>
2024-11-20 14:00:33 +0000

    add some file and folder

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+
+function NavBar() {
+  return (
+    <div className='flex justify-between items-center py-12 border-1 border-blue-50'>
+
+        
+        
+    
+
+    </div>
+  )
+}
+
+export default NavBar
